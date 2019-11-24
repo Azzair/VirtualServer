3a2d183e0d73ae10d3884b808d9160314b889b4a
Azzair <azzairazza@gmail.com>
2019-11-24 17:02:57 +0200

    hello server

--- server.go
+++ server.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+import "net"
+
+import "log"
+
+import "io"
+
 func main() {
-	fmt.Println("Hello WORLD!")
+	fmt.Println("Запускаю додаток.....")
+
+	li, err := net.Listen("tcp", ":10101")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		io.WriteString(conn, "\n Hello from TCP\n")
+		fmt.Fprintln(conn, "fmt.Hello")
+		log.Println("Відповідь надіслано")
+		conn.Close()
+	}
 }
