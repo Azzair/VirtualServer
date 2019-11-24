ad8d6a34f3a76b2459ef27f98b8320ba9e408728
Azzair <azzairazza@gmail.com>
2019-11-24 18:30:23 +0200

    Server-2

---
+++ server-02/server_02.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	fmt.Println("Запускаю додаток.....")
+
+	li, err := net.Listen("tcp", ":10101")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go hendler(conn)
+	}
+}
+
+func hendler(conn net.Conn) {
+	defer conn.Close()
+	log.Println("New connection: ", conn.RemoteAddr())
+	scan := bufio.NewScanner(conn)
+	for scan.Scan() {
+		ln := scan.Text()
+		fmt.Println(ln)
+	}
+	log.Println("Опрацьовано")
+}
